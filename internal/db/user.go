fd21ebf60745e60ea9044a4bca99c2cad81d1c4a
Ian Velder <icvelder@gmail.com>
2026-08-20 21:10:19 -0500

    added DeleteUser method for db

--- internal/db/user.go
+++ internal/db/user.go
@@ -2,9 +2,12 @@ package db
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 )
+
+var ErrIsRoot = errors.New("cannot delete the root user")
 
 type User struct {
 	ID                 int64
@@ -35,6 +38,24 @@ func (s *Store) CreateUser(ctx context.Context, username, passwordHash string, i
 		`, username, passwordHash, isAdmin)
 	if err != nil {
 		return fmt.Errorf("failed to create user: %w", err)
+	}
+	return nil
+}
+
+func (s *Store) DeleteUser(ctx context.Context, username string) error {
+	res, err := s.db.ExecContext(ctx, `
+		DELETE FROM users
+		WHERE username = ? AND is_root = 0;
+		`, username)
+	if err != nil {
+		return fmt.Errorf("failed to delete user '%s' from database: %w", username, err)
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rows == 0 {
+		return ErrIsRoot
 	}
 	return nil
 }
