0a2a61399b8fc556dbf7610588df6d6bd25ea192
Ian Velder <icvelder@gmail.com>
2026-09-15 23:22:25 -0500

    htmx starting files added in, created file server handler

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -84,6 +84,7 @@ func newServer(port string, apiCfg *api.ApiConfig, cfg *Config, cancel context.C
 	mux.Handle("/api/v1/", s.apiCfg.Auth.AuthMiddleware(apiMux))
 	mux.HandleFunc("POST /api/v1/auth/login", s.apiCfg.HandlerLogin)
 	mux.HandleFunc("GET /index", s.handlerIndex)
+	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.FS(s.staticFs))))
 	mux.HandleFunc("GET /healthz", s.handlerReadiness)
 
 	return s, nil
